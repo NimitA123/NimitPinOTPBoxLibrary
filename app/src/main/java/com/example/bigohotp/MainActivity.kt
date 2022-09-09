8562bde597ca9b50baf9a3a26c43d453bd7003ee
Nimit <nimit.agrawal0105@gmail.com>
2022-09-09 19:47:24 +0530

    Library Published

--- app/src/main/java/com/example/bigohotp/MainActivity.kt
+++ app/src/main/java/com/example/bigohotp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.bigohotp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.nimitagrawal.otpmodule.OTPScreen
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btn = findViewById<Button>(R.id.mns)
+        btn.setOnClickListener {
+            val intent = Intent(this, OTPScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
